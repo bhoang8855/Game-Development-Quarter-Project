f067a07919c7f6e9bd68c9c9549f755fa95462b2
Opsarde <Opsarde@users.noreply.github.com>
2017-05-14 16:53:08 -0700

    Added fading effect, changed title name, made button style

--- Assets/Scripts/HomeUIManager.cs
+++ Assets/Scripts/HomeUIManager.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class HomeUIManager : MonoBehaviour {
 
-	public void StartGame() {
-		SceneManager.LoadScene ("Level1");
+    public void StartGame() {
+        StartCoroutine(WaitDuration());
 	}
 
 	public void QuitGame() {
 		Application.Quit ();
-
 	}
+
+    IEnumerator WaitDuration() {
+        float floatTime = GetComponent<Fading>().BeginFade(1);
+        yield return new WaitForSeconds(floatTime);
+        SceneManager.LoadScene("Level1");
+    }
+        
 }
