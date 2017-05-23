9af20499e4b038e46f2e7617ef936da4c8436a6a
Opsarde <Opsarde@users.noreply.github.com>
2017-05-22 18:14:07 -0700

    Enemy can switch between player and tower

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -9,17 +9,36 @@ public class EnemyMovement : MonoBehaviour {
     public float moveSpeed = 4f;
     private bool isMoving;
     private Vector2 lastMove;
-    //float direction;
+    //For attack management
+    private bool isAttacking;
+    private bool isAttackingTower;
+    private float attackCooldown;
+    //Developer input parameters
+    public float cooldown;
+    public float attackRange;
+    public float playerDetectionRange;
+
 	// Use this for initialization
 	void Start () {
 		GameObject phaseSystem = GameObject.FindWithTag ("Phase System");
 		this.phaseSystemRef = (PhaseSystem) phaseSystem.GetComponent(typeof(PhaseSystem));
-        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        //target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        //target = GameObject.Find("Debugging Tower").GetComponent<Transform>();
         anim = GetComponent<Animator>();
+        isAttacking = false;
+        isAttackingTower = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if ((Vector3.Distance(transform.position, GameObject.FindWithTag("Player").GetComponent<Transform>().position) < playerDetectionRange) 
+            //&& !isAttackingTower
+            ) {
+            target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        }
+        else {
+            target = GameObject.Find("Debugging Tower").GetComponent<Transform>();
+        }
         isMoving = false;
         // two variables to hold last frame position
         float lastX = transform.position.x;
@@ -29,13 +48,36 @@ public class EnemyMovement : MonoBehaviour {
         transform.LookAt(target);
         // rotate the enemy so it will not "turn face" in 2D game
         transform.Rotate(new Vector3(0,-90,0),Space.Self);
-        // if the enemy and player has a distance greater than 0.3
-        if (Vector3.Distance(transform.position, target.position) > 0.3f) {
+
+        // Simple attack cooldown system
+        if(isAttacking) {
+            if (attackCooldown > 0) {
+                attackCooldown -= Time.deltaTime;
+            }
+            if (attackCooldown < 0) {
+                attackCooldown = 0.0f;
+            }
+        }
+        if(attackCooldown == 0) {
+            isAttacking = false;
+            attackCooldown = cooldown;
+        }
+
+        // Move to player
+        if (Vector3.Distance(transform.position, target.position) > attackRange) {
             transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
             directionX = transform.position.x - lastX;
             directionY = transform.position.y - lastY;
             lastMove = new Vector2(directionX, directionY);
             isMoving = true;
+        }
+        // Attack the player
+        else {
+            if (!isAttacking) {
+                //target.gameObject.GetComponent<PlayerHealthManager>().HurtPlayer(1);
+                //isAttackingTower = (target.name.Equals("Debugging Tower")) ? true : false;
+                isAttacking = true;
+            }
         }
         transform.rotation = Quaternion.identity;
         // Set the animation moving direction
