febffdfa08e4de505a814af2de3f1324f4556158
brian hoang <bhoang8855@sbcglobal.net>
2017-05-29 04:39:05 -0700

    Added attack animations when pressing j. Attacks dealing damage is still work in progress.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,37 +10,72 @@ public class PlayerController : MonoBehaviour {
 	private Vector2 lastMove;
 	private bool debug;
 
-	// Use this for initialization
-	void Start () {
+    private Rigidbody2D myRigidbody;
+    private bool attacking;
+    public float attackTime;
+    private float attackTimeCounter;
+
+    [SerializeField]
+    private BoxCollider2D SwordCollider;
+
+    // Use this for initialization
+    void Start () {
 		anim = GetComponent<Animator> ();
-		debug = false;		//change if not debugging
+        myRigidbody = GetComponent<Rigidbody2D>();
+        debug = false;		//change if not debugging
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		// Check if player is in motion
-		playerMoving = false;
 
-		// Controls player movement
-		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f )
-		{
-			// Move player based on A, D
-			transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f ));
-			playerMoving = true;
-			// Grabs where player was facing for idle animation
-			lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
-		}
-		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-		{
-			//Move player based on W, D
-			transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-			playerMoving = true;
-			// Grabs where player was facing for idle animation
-			lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if player is in motion
+        playerMoving = false;
 
-		//Kill enemy (for testing)
-		if (Input.GetKey(KeyCode.Space) && debug) {
+        if (!attacking)
+        {
+            // Controls player movement
+            if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+            {
+                // Move player based on A, D
+                transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+                playerMoving = true;
+                // Grabs where player was facing for idle animation
+                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            }
+            if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+            {
+                //Move player based on W, D
+                transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+                playerMoving = true;
+                // Grabs where player was facing for idle animation
+                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+            }
+
+            // Controls player attacking
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                attackTimeCounter = attackTime;
+                attacking = true;
+                myRigidbody.velocity = Vector2.zero;
+                anim.SetBool("Attack", true);
+                SwordCollider.enabled = true;
+            }
+        }
+
+        if (attackTimeCounter >= 0)
+        {
+            attackTimeCounter -= Time.deltaTime;
+        }
+
+        if (attackTimeCounter <= 0)
+        {
+            attacking = false;
+            SwordCollider.enabled = false;
+            anim.SetBool("Attack", false);
+        }
+
+        //Kill enemy (for testing)
+        if (Input.GetKey(KeyCode.Space) && debug) {
 			debugKillEnemy ();
 		}
 			
@@ -52,7 +87,12 @@ public class PlayerController : MonoBehaviour {
 		anim.SetFloat("LastMoveY", lastMove.y);
 	}
 
-	void debugKillEnemy(){
+    public void MeleeAttack()
+    {
+        SwordCollider.enabled = !SwordCollider.enabled;
+    }
+
+    void debugKillEnemy(){
 		GameObject target = GameObject.Find("Enemy Unit(Ground)(Clone)");
 		if (target != null) {
 			EnemyMovement targetRef = (EnemyMovement)target.GetComponent (typeof(EnemyMovement));
---
+++ Assets/Scripts/SwordCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCollider : MonoBehaviour
+{
+
+    public int damageToGive;
+    public GameObject damageBurst;
+    public Transform hitPoint;
+    //	public GameObject dmg;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.isTrigger != true && other.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToGive);
+            // Enable damage burst while hitting the enemy
+            Instantiate(damageBurst, transform.position, transform.rotation);
+            //			// Show damage numbers
+            //			var clone = (GameObject) Instantiate (dmg, transform.position, Quaternion.Euler (Vector3.zero));
+            //			clone.GetComponent<FloatingNumbers>().dmg = damageToGive;
+        }
+        if (other.gameObject.tag == "Rock")
+        {
+            other.gameObject.GetComponent<RockHealthManager>().HurtRock(damageToGive);
+            // Enable damage burst while hitting the enemy
+            Instantiate(damageBurst, transform.position, transform.rotation);
+        }
+    }
+}
