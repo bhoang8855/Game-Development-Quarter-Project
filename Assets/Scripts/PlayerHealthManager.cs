ac204bbf19d34dcbc0a25625412aa2183e68623b
brian hoang <bhoang8855@sbcglobal.net>
2017-06-01 20:09:39 -0700

    'Wider camera view. Player's health no longer goes below zero when dying'

--- Assets/Scripts/PlayerHealthManager.cs
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -28,6 +28,7 @@ public class PlayerHealthManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (playerCurrentHealth <= 0) {
+            playerCurrentHealth = 0;
 			gameObject.SetActive (false);
             AudioSource.PlayClipAtPoint(sound, transform.position);
             GameOverText.SetActive(true);
